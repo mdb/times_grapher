66a752e785185b0801b25a1375ed2285567892f8
Mike Ball <mikedball@gmail.com>
2012-04-06 17:28:25 -0400

    Created rudimentary overlay.

--- public/javascript/tg.js
+++ public/javascript/tg.js
@@ -2,7 +2,7 @@ if (typeof TG === 'undefined' || !TG) {
   var TG = {};
 }
 
-TG.lightbox = (function ($) {
+TG.overlay = (function ($) {
   var _self;
 
   _self = {
@@ -18,6 +18,33 @@ TG.lightbox = (function ($) {
     },
 
     open: function (overlay) {
+        var mask = $('#overlay-mask');
+
+        mask.fadeIn('1000');
+        _self.positionOverlay(overlay);
+        overlay.fadeIn('fast');
+        _self.setUpCloseClickHandlers(mask, overlay);
+        _self.setUpCloseClickHandlers(overlay.find('.close'), overlay);
+    },
+
+    positionOverlay: function (overlay) {
+        var winH = $(window).height(),
+            winW = $(window).width();
+
+        //Set the popup window to center
+        overlay.css('top',  winH/2 - overlay.height()/2);
+        overlay.css('left', winW/2 - overlay.width()/2);
+    },
+
+    setUpCloseClickHandlers: function ($elem, overlay) {
+        $elem.click(function () {
+            _self.close(overlay);
+        });
+    },
+
+    close: function (overlay) {
+        $('#overlay-mask').hide();
+        overlay.hide();
     }
   };
 
