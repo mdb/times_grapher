ef17e1947de67b08511bbafc3f43f90ac9af74c0
Mike Ball <mikedball@gmail.com>
2014-01-20 13:14:58 -0500

    initial rails refactor commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimesGrapher::Application.config.secret_key_base = '48f1b9b9c292f31fcdce4614b691f4de83e2be3d794dabe43ba62d09e723313ca4028bc869658081ca2a2d30ff372a7e6d6d23cc534fa4e62cfb6d5f01e08102'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimesGrapher::Application.config.session_store :cookie_store, key: '_times_grapher_session'
