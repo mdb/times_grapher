fed51d75c2bbb36d416a32eef42488e37628be3d
Mike Ball <mikedball@gmail.com>
2014-01-21 13:43:13 -0500

    ApiController now forces json request format

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,15 @@
 class ApiController < ApplicationController
+  before_filter :force_json
+
   def index
     year = params[:year] ? params[:year] : '2014'
 
     @queries = TimesQueryCollection.new([params[:terms]].flatten, :year => year)
+  end
+
+  private
+
+  def force_json
+    request.format = :json
   end
 end
