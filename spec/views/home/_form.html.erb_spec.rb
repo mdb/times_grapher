5e69914ecb6ac5452b501fd9dc2a592281d31f87
Mike Ball <mikedball@gmail.com>
2014-01-31 08:11:58 -0500

    adds form partial

---
+++ spec/views/home/_form.html.erb_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe "home/_form.html.erb" do
+  before(:each) do
+    render '/home/form'
+  end
+
+  it "renders the proper root form element" do
+    rendered.should have_selector "form"
+  end
+
+  it "renders a fieldset" do
+    rendered.should have_selector "form fieldset"
+  end
+
+  it "renders a first search input" do
+    rendered.should have_selector "form input[name=search1]"
+  end
+
+  it "renders a second search input" do
+    rendered.should have_selector "form input[name=search2]"
+  end
+
+  it "renders an option selector" do
+    rendered.should have_selector "form select[name=year]"
+  end
+
+  it "renders a 2011 - 1981 options selectors" do
+    rendered.should have_selector "option"
+  end
+
+  it "renders a submit input" do
+    rendered.should have_selector "input.submit"
+  end
+end
